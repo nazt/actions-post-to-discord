8ebecff53b8661cf740d51043a5eef2adc60c5c8
Nat Weerawan <nat.wrw@gmail.com>
2024-01-09 14:25:00 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,14 @@ set -e
 
 
 # Fetching inputs
-WEBHOOK_URL=$INPUT_WEBHOOK_URL
-REPO_NAME=$INPUT_REPO_NAME
-COMMIT_HASH=$INPUT_COMMIT_HASH
-REF_NAME=$INPUT_REF_NAME
-MACHINE=$INPUT_MACHINE
+WEBHOOK_URL="$INPUT_WEBHOOK_URL"
+REPO_NAME="$INPUT_REPO_NAME"
+COMMIT_HASH="$INPUT_COMMIT_HASH"
+REF_NAME="$INPUT_REF_NAME"
+MACHINE="$INPUT_MACHINE"
 
+
+env
 # Construct the JSON payload
 PAYLOAD=$(jq -nc   --arg description "Deploying Repository: $REPO_NAME on branch $REF_NAME"   --arg commit_hash "$COMMIT_HASH"   --arg machine "$MACHINE"   --arg ref_name "$REF_NAME"   --arg ip "$(curl -s https://api.ipify.org)"   '{"embeds": [{"description": $description, "fields": [
     {"name": "Commit Hash", "value": $commit_hash},
