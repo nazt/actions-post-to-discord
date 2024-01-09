abde9e3952fe2a947493a5f88266f6c1543c02bb
Nat Weerawan <nat.wrw@gmail.com>
2024-01-09 14:37:51 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,24 +6,17 @@ set -e
 
 
 # Fetching inputs
-WEBHOOK_URL="$INPUT_WEBHOOK_URL"
-REPO_NAME="$INPUT_REPO_NAME"
-COMMIT_HASH="$INPUT_COMMIT_HASH"
-REF_NAME="$INPUT_REF_NAME"
-MACHINE="$INPUT_MACHINE"
-
-
 env
 # Construct the JSON payload
-PAYLOAD=$(jq -nc   --arg description "Deploying Repository: $REPO_NAME on branch $REF_NAME"   --arg commit_hash "$COMMIT_HASH"   --arg machine "$MACHINE"   --arg ref_name "$REF_NAME"   --arg ip "$(curl -s https://api.ipify.org)"   '{"embeds": [{"description": $description, "fields": [
+PAYLOAD=$(jq -nc   --arg description "Deploying Repository: $INPUT_REPO_NAME on branch $INPUT_REF_NAME"   --arg commit_hash "$INPUT_COMMIT_HASH"   --arg machine "$INPUT_MACHINE"   --arg ref_name "$INPUT_REF_NAME"   --arg ip "$(curl -s https://api.ipify.org)"   '{"embeds": [{"description": $description, "fields": [
     {"name": "Commit Hash", "value": $commit_hash},
     {"name": "Ref Name", "value": $ref_name},
     {"name": "Machine", "value": $machine},
     {"name": "IP Address", "value": $ip}
   ]}]}')
 
-echo "$WEBHOOK_URL"
+echo "$INPUT_WEBHOOK_URL"
 echo "$INPUT_WEBHOOK_URL"
 
 # Post to Discord
-curl -X POST -H "Content-Type: application/json" -d "$PAYLOAD" "$WEBHOOK_URL"
+curl -X POST -H "Content-Type: application/json" -d "$PAYLOAD" "$INPUT_WEBHOOK_URL"
