52b62f2d94eaa944a5fa25e2ee6607a780f6e42b
Nat Weerawan <nat.wrw@gmail.com>
2024-01-09 14:47:44 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,13 @@
 # set -x
 set -e
 
-
-
-# Fetching inputs
-env
 # Construct the JSON payload
-PAYLOAD=$(jq -nc   --arg description "Deploying Repository: $INPUT_REPO_NAME on branch $INPUT_REF_NAME"   --arg commit_hash "$INPUT_COMMIT_HASH"   --arg machine "$INPUT_MACHINE"   --arg ref_name "$INPUT_REF_NAME"   --arg ip "$(curl -s https://api.ipify.org)"   '{"embeds": [{"description": $description, "fields": [
+PAYLOAD=$(jq -nc   --arg description "Deploying Repository: $GITHUB_REPOSITORY on branch $GITHUB_REF_NAME"   --arg commit_hash "$GITHUB_SHA"   --arg machine "$RUNNER_NAME"   --arg ref_name "$GITHUB_REF_NAME"   --arg ip "$(curl -s https://api.ipify.org)"   '{"embeds": [{"description": $description, "fields": [
     {"name": "Commit Hash", "value": $commit_hash},
     {"name": "Ref Name", "value": $ref_name},
     {"name": "Machine", "value": $machine},
     {"name": "IP Address", "value": $ip}
   ]}]}')
-
-echo "$INPUT_DISCORD_WEBHOOK_URL"
-echo "$INPUT_DISCORD_WEBHOOK_URL"
 
 # Post to Discord
 curl -X POST -H "Content-Type: application/json" -d "$PAYLOAD" "$INPUT_DISCORD_WEBHOOK_URL"
